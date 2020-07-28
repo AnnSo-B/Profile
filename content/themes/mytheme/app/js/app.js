59652dcb8be69c4bb3ac26ac9beacbef2f720444
AnnSo-B <as.baujard.33@gmail.com>
2020-07-28 17:50:15 +0200

    add smooth scroll on action links

--- content/themes/mytheme/app/js/app.js
+++ content/themes/mytheme/app/js/app.js
@@ -11,6 +11,11 @@ const app = {
     app.$bannerElement = $('.banner');
     app.$headerMenuElement = $('.header__menu');
     app.initScrollex();
+
+    /**
+     * Smooth scroll
+     */
+    app.initSmoothScroll();
   },
 
   /**
@@ -32,6 +37,43 @@ const app = {
   setMenuFixed: () => {
     // https://api.jquery.com/addClass/#addClass-className
     app.$headerMenuElement.addClass('fixed')
+  },
+
+  /**
+   * Smooth scroll methods
+   */
+  initSmoothScroll: () => {
+    const actionLinks = document.querySelectorAll('.action-link');
+
+    for (const actionLink of actionLinks) {
+      actionLink.addEventListener('click', app.handleSmoothScroll);
+    };
+  },
+
+  handleSmoothScroll: (event) => {
+    event.preventDefault();
+
+    // object.hash => get href value
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLHyperlinkElementUtils/hash  
+    const $currentTarget = $(event.target.hash);
+
+    if ($currentTarget.length) {
+      // need to take the header__menu into account when scroll to section
+      const headerMenuElementHeight = app.$headerMenuElement.outerHeight();
+
+      // https://api.jquery.com/offset/#offset
+      const currentTargetPosition = $currentTarget.offset().top - headerMenuElementHeight * 1.5;
+
+      // I want to wait 2 secondes before displaying this section depending on the body
+      // https://api.jquery.com/animate/
+      $('html, body').animate(
+        {
+          // https://developer.mozilla.org/fr/docs/Web/API/Element/scrollTop
+          scrollTop: currentTargetPosition
+        },
+        2000
+      );
+    }
   },
 
 };
