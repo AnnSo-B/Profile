448530313c5b3f8f509dbdbd396c4465801d2f9e
AnnSo-B <as.baujard.33@gmail.com>
2020-07-28 17:36:07 +0200

    toggle fixed class on header__menu on scroll

--- content/themes/mytheme/app/js/app.js
+++ content/themes/mytheme/app/js/app.js
@@ -1,7 +1,39 @@
-var app = {
-  init: function() {
+// https://www.npmjs.com/package/jquery.scrollex#usage
+require('jquery.scrollex');
+
+const app = {
+  init: () => {
     console.log('init');
-  }
+
+    /**
+     * Menu on Scroll
+     */
+    app.$bannerElement = $('.banner');
+    app.$headerMenuElement = $('.header__menu');
+    app.initScrollex();
+  },
+
+  /**
+   * Menu on Scroll methods
+   */
+  initScrollex: () => {
+    // https://www.npmjs.com/package/jquery.scrollex#usage
+    app.$bannerElement.scrollex({
+      enter: app.setMenuUnfixed,
+      leave: app.setMenuFixed
+    })
+  },
+
+  setMenuUnfixed: () =>  {
+    //https://api.jquery.com/removeClass/#removeClass-className
+    app.$headerMenuElement.removeClass('fixed')
+  },
+
+  setMenuFixed: () => {
+    // https://api.jquery.com/addClass/#addClass-className
+    app.$headerMenuElement.addClass('fixed')
+  },
+
 };
 
-$(app.init);
+document.addEventListener('DOMContentLoaded', app.init);
