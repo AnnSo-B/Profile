77b83b1efcb6644c94e5871e5ce27cdc85e86c57
AnnSo-B <as.baujard.33@gmail.com>
2020-08-01 15:17:43 +0200

    grid dynamization - fontawesome tag excluded

--- content/themes/mytheme/front-page.php
+++ content/themes/mytheme/front-page.php
@@ -18,8 +18,23 @@
   <?php endwhile; endif; ?>
 </section>
 
-      <section id="grid" class="grid">
-        <?php get_template_part('template-parts/front-page/grid') ;?>
-      </section>
+<section id="grid" class="grid">
+  <?php 
+    // custom loop to get posts
+    $args = [
+      'post-type'         => 'post', // we want to get the post elements - https://developer.wordpress.org/reference/classes/wp_query/#post-type-parameters
+      'posts_per_page'    => 6, // we want the 6 last posts to begin with https://developer.wordpress.org/reference/classes/wp_query/#pagination-parameters 
+      'order'             => 'ASC', // https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters - by default by date
+      'category_name'     => 'competences' // we don't want the article from the "competence" - take a look at the url when hover category name in BO to get its id https://developer.wordpress.org/reference/classes/wp_query/#category-parameters
+    ];
+
+   // wp_query instanciation
+   $skills = new WP_Query($args);
+  ?>
+  <?php if ($skills->have_posts()) : while ($skills->have_posts()) : $skills->the_post(); ?>
+    <?php get_template_part('template-parts/front-page/grid') ;?>
+  <?php endwhile; endif; ?>
+
+</section>
 
 <?php get_footer(); ?>
--- content/themes/mytheme/template-parts/front-page/grid.php
+++ content/themes/mytheme/template-parts/front-page/grid.php
@@ -1,30 +1,6 @@
         <div class="grid__item">
+          <!-- !TODO : find a way to customize fontawesome -->
           <i class="grid__item__icon fa fa-user" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Toujours à l'écoute</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div class="grid__item">
-          <i class="grid__item__icon fa fa-arrow-right" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Pro-active</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div class="grid__item">
-          <i class="grid__item__icon fa fa-bullhorn" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Communicante</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div class="grid__item">
-          <i class="grid__item__icon fa fa-graduation-cap" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Des compétences reconnues</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div class="grid__item">
-          <i class="grid__item__icon fa fa-code" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Expérimentée</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div class="grid__item">
-          <i class="grid__item__icon fa fa-heart" aria-hidden="true"></i>
-          <h3 class="grid__item__title">Avec passion</h3>
-          <p class="grid__item__content">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h3 class="grid__item__title"><?php the_title(); ?></h3>
+          <?php the_content(); ?>
         </div>
