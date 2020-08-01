5306789210d8cb9fd678779f745fa03d98acfcdc
AnnSo-B <as.baujard.33@gmail.com>
2020-08-01 14:42:33 +0200

    posts section dynamization

--- content/themes/mytheme/front-page.php
+++ content/themes/mytheme/front-page.php
@@ -1,8 +1,23 @@
 <?php get_header(); ?>
 
-      <section id="posts" class="posts">
-        <?php get_template_part('template-parts/front-page/post-excerpt') ;?>
-      </section>
+<section id="posts" class="posts">
+  <?php 
+    // custom loop to get posts
+    $args = [
+      'post-type'         => 'post', // we want to get the post elements - https://developer.wordpress.org/reference/classes/wp_query/#post-type-parameters
+      'posts_per_page'    => 6, // we want the 6 last posts to begin with https://developer.wordpress.org/reference/classes/wp_query/#pagination-parameters 
+      'order'             => 'ASC', // https://developer.wordpress.org/reference/classes/wp_query/#order-orderby-parameters - by default by date
+      'category__not_in'  => 2 // we don't want the article from the "competence" - take a look at the url when hover category name in BO to get its id https://developer.wordpress.org/reference/classes/wp_query/#category-parameters
+    ];
+
+   // wp_query instanciation
+   $posts = new WP_Query($args);
+  ?>
+  <?php if ($posts->have_posts()) : while ($posts->have_posts()) : $posts->the_post(); ?>
+    <?php get_template_part('template-parts/front-page/post-excerpt') ;?>
+  <?php endwhile; endif; ?>
+</section>
+
       <section id="grid" class="grid">
         <?php get_template_part('template-parts/front-page/grid') ;?>
       </section>
--- content/themes/mytheme/template-parts/front-page/post-excerpt.php
+++ content/themes/mytheme/template-parts/front-page/post-excerpt.php
@@ -1,24 +1,9 @@
-        <article class="post" style="background-image: url('../images/neige.jpg');">
-          <h2 class="post__title">Winter View</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
-        </article>
-        <article class="post" style="background-image: url('../images/arbre.jpg');">
-          <h2 class="post__title">On the fly</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
-        </article>
-        <article class="post" style="background-image: url('../images/montagne.jpg');">
-          <h2 class="post__title">Horizon</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
-        </article>
-        <article class="post" style="background-image: url('../images/fete.jpg');">
-          <h2 class="post__title">Party tour</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
-        </article>
-        <article class="post" style="background-image: url('../images/rue.jpg');">
-          <h2 class="post__title">L'agence d'à côté</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
-        </article>
-        <article class="post" style="background-image: url('../images/soleil.jpg');">
-          <h2 class="post__title">Sunlight</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempora cupiditate quo delectus cum minima illum quod ipsam. Laborum totam et aliquam, ex temporibus earum. Eos, in sunt. Possimus ut, adipisci totam dolores quam soluta facilis libero quidem nemo voluptas.</p>
+        <article
+          class="post"
+          <?php if (the_post_thumbnail_url() !== ''); ?>
+            style="background-image: url('<?php the_post_thumbnail_url(); ?>');"
+          <?php ?>
+        >
+          <h2 class="post__title"><?php the_title(); ?></h2>
+          <?php the_excerpt(); ?>
         </article>
