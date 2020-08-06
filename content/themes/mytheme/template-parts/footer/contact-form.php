ff3260498714bd1a56fff7a241ff14fc87d8c8b5
AnnSo-B <as.baujard.33@gmail.com>
2020-08-06 16:49:05 +0200

    use Contact Form 7 for the contact-form - ! css change + wp-config change

--- content/themes/mytheme/template-parts/footer/contact-form.php
+++ content/themes/mytheme/template-parts/footer/contact-form.php
@@ -1,4 +1,5 @@
-        <form action="#" class="contact-form">
+ <!-- USING CONTACT FORM 7 This :  -->
+ <!-- <form action="#" class="contact-form">
           <div class="user-info">
             <div class="field">
               <label class="field__label" for="name">Nom</label>
@@ -14,4 +15,27 @@
             <textarea class="field__input" name="message" id="message" cols="30" rows="5"></textarea>
           </div>
           <button class="contact-form__button" type="submit">Envoyer</button>
-        </form>
+        </form> -->
+<!-- becomes this in WP backoffice CF7 extensions : 
+  <div class="user-info">
+    <div class="field">
+      <label class="field__label" for="name">Nom</label>
+      [text* your-name]
+    </div>
+    <div class="field">
+      <label class="field__label"for="email">Email</label>
+      [email* your-email]
+    </div>
+  </div>
+  <div class="field">
+    <label class="field__label" for="message">Message</label>
+    [textarea your-message]
+  </div>
+  [submit class=contact-form__button "Envoyer"]
+-->
+<!-- and it's displayed this way thanks to the shortcode -->
+<div class="contact-form">
+  <?php
+    echo do_shortcode( '[contact-form-7 id="47" title="Contact form 1"]' );
+  ?>
+</div>
