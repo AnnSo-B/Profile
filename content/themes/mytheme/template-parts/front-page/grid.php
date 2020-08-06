755f43f7c1bf0a421bae5f59f5e402d484974f9f
AnnSo-B <as.baujard.33@gmail.com>
2020-08-06 15:51:46 +0200

    grid dynamization - use advanced custom fields and acf font awesome to display icons

--- content/themes/mytheme/template-parts/front-page/grid.php
+++ content/themes/mytheme/template-parts/front-page/grid.php
@@ -1,6 +1,14 @@
         <div class="grid__item">
-          <!-- !TODO : find a way to customize fontawesome -->
-          <i class="grid__item__icon fa fa-user" aria-hidden="true"></i>
+          <!-- 
+            customize fontawesome : using advanced custom fields and advanced custom fields font awesome 
+            ACF / font awesome settings -> version 4
+            ACF / Groupes de champs / Ajouter -> add a new group
+            Ajouter un champ -> create icon field / Type de champ = Font Awesome Icon / Valeur de retour = Icon Class here
+            as we want this field to be displayed only in this grid we add a condition 
+            now we find this new field in the articles having the "competences" category and we can choose which icon we want to display for each posts
+            to display it in this template we use the_field() which comes with ACF
+          -->
+          <i class="grid__item__icon fa <?php the_field('icon'); ?>" aria-hidden="true"></i>
           <h3 class="grid__item__title"><?php the_title(); ?></h3>
           <?php the_content(); ?>
         </div>
