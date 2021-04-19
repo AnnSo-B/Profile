4f4553e690cafff8dc864af0cb7cdbdbe3df662f
AnnSo-B <as.baujard.33@gmail.com>
2021-04-19 17:21:28 +0200

    register Project Custom Type

--- content/plugins/projects-plugin/projects-plugin.php
+++ content/plugins/projects-plugin/projects-plugin.php
@@ -9,3 +9,67 @@
 if (!defined('WPINC')) {
   die();
 };
+
+class Projects {
+  public function __construct()
+  {
+    # code...
+    // on attache l'instanciation de la classe au hook d'initialisation (on instancie la classe après celle-ci)
+    add_action('init', [$this, 'register_cpt']);
+  }
+
+  /**
+   * Pour déclarer le custom post type
+   * https://wordpress.org/support/article/post-types/
+   * 
+   * @return void
+   */
+  public function register_cpt() {
+    // les nomenclatures dans le BO
+    // https://developer.wordpress.org/reference/functions/get_post_type_labels/
+    $labels = [
+      'name'                => 'Projets',
+      'singular_name'       => 'Projet',
+      'menu_name'           => 'Projets',
+      'name_admin_bar'      => 'Projet',
+      'add_new'             => 'Ajouter un projet',
+      'add_new_item'        => 'Ajouter un nouveau projet',
+      'new_item'            => 'Nouveau projet',
+      'edit_item'           => 'Editer un projet',
+      'view_item'           => 'Voir le projet',
+      'all_items'           => 'Voir tous les projets',
+      'search_items'        => 'Rechercher un projet',
+      'not_found'           => 'Aucun projet toruvé',
+      'not_found_in_trash'  => 'Aucun projet trouvé dans la corbeille',
+    ];
+
+    // les arguments 
+    $args = [
+      // minimum vital pour que ça s'affiche dans le BO
+      'labels' => $labels, // ci-dessus
+      'public' => true, // permettra l'affichage de l'entrée dans le menu du BO
+
+      // options facultatives
+      'hierarchical' => false, // determine si on aura une hiérarchie par exemple des projets qui auraient des sous-projets
+      'menu_position' => 6, // détermine la position dans le menu
+      'menu_icon'  => 'dashicons-art', // via les dash icons équivalent de fontawesome pour WP https://developer.wordpress.org/resource/dashicons/#buddicons-tracking
+      'supports' => [
+        'title',
+        'editor', // pas guttenberg par défaut, il faut ajouter l'api de wordpress pour activer guttenberg
+        'excerpt',
+        'custom-fields',
+        'thumbnail'
+      ], // champs natifs supportés
+      'show_in_rest' => true, // voir éditor ci-dessus - on recevra également les données via l'API
+      'has_archive' => true, // permet de gérer le CPT dans les templates d'archives
+      'rewrite' => [
+        'slug' => 'projets'
+      ] // permet la réécriture d'url par défaut ce serait /project/nom-du-projet --> ça deviendra ce qu'on indique ici : /projet/nom-du projet 
+    ];
+
+    // https://developer.wordpress.org/reference/functions/register_post_type/
+    register_post_type('project', $args);
+  }
+};
+
+$projects = new Projects();
